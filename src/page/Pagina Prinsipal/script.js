d021ae175cdcf84265304948b36fb282bbfcd306
jhonnyer512 <kakaroto639@outlook.com>
2025-09-10 18:17:37 -0500

    new model

--- src/page/Pagina Prinsipal/script.js
+++ src/page/Pagina Prinsipal/script.js
@@ -1,17 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const sidebar = document.querySelector(".sidebar");
-    const mainContent = document.querySelector(".main-content");
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
 
-    menuToggle.addEventListener("click", function () {
-        sidebar.classList.toggle("active");
-        mainContent.classList.toggle("menu-open");
-
-        // Mueve el botón junto con el sidebar
-        if (sidebar.classList.contains("active")) {
-            menuToggle.style.left = "260px"; // Lo posicionamos afuera del sidebar
-        } else {
-            menuToggle.style.left = "10px"; // Lo regresamos a su posición original
-        }
-    });
-});
